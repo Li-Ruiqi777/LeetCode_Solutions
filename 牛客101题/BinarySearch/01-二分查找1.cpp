4e02aacb221f5ac859dc336eadfd385b8494edb7
Li-Ruiqi <1009962019@qq.com>
2024-07-16 16:35:19 +0800

    update

---
+++ 牛客101题/BinarySearch/01-二分查找1.cpp
@@ -0,0 +1,34 @@
+// 一刷：2024-7-16，太久没写二叉搜索了，完全忘记咋写了...这题基本上是背模板写出来的，主要就是注意一下
+// 左右边界的更新条件、while循环的退出条件
+
+class Solution
+{
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param nums int整型vector
+     * @param target int整型
+     * @return int整型
+     */
+    int search(vector<int> &nums, int target)
+    {
+        // write code here
+        if (nums.empty())
+            return -1;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+};
