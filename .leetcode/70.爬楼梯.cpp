a196659c1677c259156c9010f57c7096bedc16bf
Li-Ruiqi <1009962019@qq.com>
2025-02-19 09:27:12 +0800

    2.18日：刷了2题

---
+++ .leetcode/70.爬楼梯.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=cpp
+ *
+ * [70] 爬楼梯
+ */
+#include <vector>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        if (n == 1)
+            return 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; ++i)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
